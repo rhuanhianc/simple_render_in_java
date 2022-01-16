2147fe2bfac675b4ad9d75b7b99b446a41be045a
Rhuan Hianc <rhuanhianc123@gmail.com>
2022-01-15 23:37:25 -0300

    Create Triangle.java

---
+++ forms/Triangle.java
@@ -0,0 +1,19 @@
+package forms;
+
+import javafx.scene.paint.Color;
+import vertex.Vertex;
+
+public class Triangle {
+    Vertex v1;
+    Vertex v2;
+    Vertex v3;
+    Color color;
+    Triangle(Vertex v1, Vertex v2, Vertex v3, Color color){
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.color = color;
+    }
+    
+    
+}

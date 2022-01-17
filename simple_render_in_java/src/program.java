9944c60cfa110038b8783060c0716da781056d3a
Rhuan Hianc <rhuanhianc123@gmail.com>
2022-01-17 18:56:52 -0300

    att0105

---
+++ simple_render_in_java/src/program.java
@@ -0,0 +1,15 @@
+
+import javax.swing.JFrame;
+
+import frame.FrameClass;
+
+
+class Program {
+    public static void main(String[] args) {
+      JFrame frame = new JFrame();
+      FrameClass frameClass = new FrameClass();
+      frameClass.conteiner(frame.getContentPane());
+      frame.setSize(400, 400);
+      frame.setVisible(true);
+    }
+  }
---
+++ simple_render_in_java/src/vertex/Vertex.java
@@ -0,0 +1,12 @@
+package vertex;
+
+public class Vertex {
+    public double x;
+    public  double y;
+    public double z;
+    public Vertex(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
